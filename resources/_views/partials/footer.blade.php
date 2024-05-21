49f431adebd9e172b3b4c2853cf8a55b4a027942
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-21 15:58:01 +0200

    add auth

---
+++ resources/_views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center fixed-bottom bg-dark p-3">
+    <div class="container text-center text-white">
+        Footer
+    </div>
+</footer>
---
+++ resources/_views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header class="text-danger p-3 bg-dark mb-5 hv">
+    <div class="containers">
+        <nav class="navbar navbar-expand-sm navbar-light px-3">
+            <div class="px-3">
+                <h3>GDR Weapon</h3>
+            </div>
+            <div class="navbar-nav px-3">
+                <a class="text-danger " aria-current="page" href="#">Home</a>
+            </div>
+            <div class="navbar-nav px-3">
+                <a class=" text-danger" aria-current="page" href="{{ route('weapons.index') }}">Weapons</a>
+            </div>
+            <div class="navbar-nav px-3">
+                <a class=" text-danger" aria-current="page" href="{{ route('characters.index') }}">Characters</a>
+            </div>
+        </nav>
+    </div>
+</header>
