4985df9b4552b47bd1d9262c3df6488387cd9438
danielecolaci <danielecolaci@outlook.com>
2024-05-21 13:34:01 +0200

    Fix Show page style

--- resources/views/weapons/show.blade.php
+++ resources/views/weapons/show.blade.php
@@ -2,27 +2,27 @@
 
 
 @section('content')
-    <div class="container text-center">
+    <div class="container text-center mt-5">
         <h1>GDR Weapon</h1>
-        <div class="row">
-            <div class="col g-4">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-8 col-lg-6">
                 <div class="card">
-                    <div class="card-title">
-                        <h3>{{ $weapon->name }}</h3>
+                    <div class="card-header bg-dark text-white">
+                        <h3 class="card-title">{{ $weapon->name }}</h3>
                     </div>
                     <div class="card-body">
-                        <h5>Slug: {{ $weapon->slug }}</h5>
-                        <h5>Type: {{ $weapon->type }}</h5>
-                        <h5>Category: {{ $weapon->category }}</h5>
-                        <h5>Weight: {{ $weapon->weight }}</h5>
-                        <h5>Cost: {{ $weapon->cost }}</h5>
-                        <h5>Damage: {{ $weapon->damage_dice }}</h5>
+                        <p class="card-text"><b>Slug:</b> {{ $weapon->slug }}</p>
+                        <p class="card-text"><b>Type:</b> {{ $weapon->type }}</p>
+                        <p class="card-text"><b>Category:</b> {{ $weapon->category }}</p>
+                        <p class="card-text"><b>Weight:</b> {{ $weapon->weight }}</p>
+                        <p class="card-text"><b>Cost:</b> {{ $weapon->cost }}</p>
+                        <p class="card-text"><b>Damage:</b> {{ $weapon->damage_dice }}</p>
                     </div>
                 </div>
-                <div class="card-footer">
-                    <a class="btn btn-dark" href="{{ route('weapons.index') }}">Back</a>
-                </div>
 
+                <div class="card-footer">
+                    <a class="btn btn-danger w-100 mt-5" href="{{ route('weapons.index') }}">Back</a>
+                </div>
             </div>
 
         </div>
