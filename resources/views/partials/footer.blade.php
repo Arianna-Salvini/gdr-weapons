04703ac37fb9c945bf5f2096380dfc613b93935f
Daniele <danielecolaci@outlook.com>
2024-05-28 19:00:00 +0200

    Fix Header Style. Hide Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="text-center fixed-bottom bg-dark p-3">
+{{-- <footer class="text-center fixed-bottom bg-dark p-3">
     <div class="container text-center text-white">
         Footer
     </div>
-</footer>
+</footer> --}}
